8c881d12bbe1a556b3feceb3caf9d19facc9410a
David Federspiel <david@federnet.com>
2021-01-05 21:25:02 -0500

    Add project files.

---
+++ MyNewConsoleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using MyProjectWithModels;
+using Newtonsoft.Json.Schema.Generation;
+using System;
+
+
+/// <summary>
+/// Using Json Schema Generator, we can quickly convert .Net classes to JSON Schema Definitions for use in validation.
+/// </summary>
+namespace MyNewConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello JSchemaGenerator!");
+
+            var generater = new JSchemaGenerator();
+            var schema1 = generater.Generate(typeof(MyCustomType1));
+            var schema2 = generater.Generate(typeof(MyCustomType2));
+            var schema3 = generater.Generate(typeof(MyCustomType3));
+            var schema4 = generater.Generate(typeof(MyCustomType4));
+            var schema5 = generater.Generate(typeof(MyCustomType5));
+            var schema6 = generater.Generate(typeof(MyCustomType6));
+            var schema7 = generater.Generate(typeof(MyCustomType7));
+            var schema8 = generater.Generate(typeof(ThingOne));
+            var schema9 = generater.Generate(typeof(ThingTwo));
+            var schema10 = generater.Generate(typeof(ThingThree));
+
+            Console.WriteLine(schema1);
+            Console.WriteLine(schema2);
+            Console.WriteLine(schema3);
+            Console.WriteLine(schema4);
+            Console.WriteLine(schema5);
+            Console.WriteLine(schema6);
+            Console.WriteLine(schema7);
+            Console.WriteLine(schema8);
+            Console.WriteLine(schema9);
+            Console.WriteLine(schema10);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MyProjectWithModels/Class1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+
+/// <summary>
+/// Classes to convert with NewtonSoft JSON Schema Generator
+/// </summary>
+namespace MyProjectWithModels
+{
+    public struct MyCustomType1
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<string> MyFancyCollection { get; set; }
+    }
+
+    public struct MyCustomType2
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public struct MyCustomType3
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public struct MyCustomType4
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public struct MyCustomType5
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public struct MyCustomType6
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public struct MyCustomType7
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class ThingOne
+    {
+        public string FancyString { get; set; }
+        public string NumberAsString { get; set; }
+        public bool? IsActive { get; set; }
+        public bool Deleted { get; set; }
+    }
+
+    public class ThingTwo
+    {
+        public int ThingId { get; set; }
+        public ThingOne ThingOne { get; set; }
+    }
+
+    public class ThingThree
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string NumberAsString { get; set; }
+        public bool IsActive { get; set; }
+        public bool Deleted { get; set; }
+        public static Expression<Func<ThingTwo, ThingThree>> FromRetreaderRetreadShopMappings => someRandomMapping => new ThingThree
+        {
+            Id = someRandomMapping.ThingId,
+            Name = someRandomMapping.ThingOne != null ? someRandomMapping.ThingOne.FancyString : null,
+            NumberAsString = someRandomMapping.ThingOne != null ? someRandomMapping.ThingOne.NumberAsString : null,
+            IsActive = someRandomMapping.ThingOne != null ? someRandomMapping.ThingOne.IsActive ?? false : false,
+            Deleted = someRandomMapping.ThingOne != null ? someRandomMapping.ThingOne.Deleted : false,
+        };
+    }
+}
